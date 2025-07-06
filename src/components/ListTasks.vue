c536806128adc48f97710e1939c99a2de004bbdb
Egor Prokhorov <qiwibally@mail.ru>
2025-07-06 12:44:58 +0000

    рефактор

--- src/components/ListTasks.vue
+++ src/components/ListTasks.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
     import { storeToRefs } from 'pinia';
     import { useTasksStore } from '../stores/tasks';
-    import { computed } from 'vue';
 
     const tasksStore = useTasksStore();
-    const { tasks, currentPage } = storeToRefs(tasksStore);
-    const { setIsDone, perPage } = tasksStore; 
-
-    const tasksPerPage = computed(() => {
-        const start = (currentPage.value - 1) * perPage;
-        return tasks.value.slice(start, start + perPage);
-    });
+    const { paginated } = storeToRefs(tasksStore);
+    const { setIsDone } = tasksStore; 
 </script>
 
 <template>
-    <div class="tasks-box" v-for="task in tasksPerPage" :key="task.id">
+    <div class="tasks-box" v-for="task in paginated" :key="task.id">
        {{ task.text }}
 
        <my-button
